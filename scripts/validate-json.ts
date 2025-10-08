1cbd6dce22ced2562cef3543b2d2ed763d102fad
Abhinav <119372409+Abhinav5383@users.noreply.github.com>
2025-10-08 06:56:46 +0530

    Add JSON validation script

---
+++ scripts/validate-json.ts
@@ -0,0 +1,11 @@
+import jsonData from "./versions.json";
+
+function main() {
+  try {
+    JSON.parse(jsonData);
+  } catch (err) {
+    console.error("The JSON is invalid!", err);
+  }
+}
+
+main();
